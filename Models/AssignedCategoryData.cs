53aa2bb1c77212b908d5f65a34206dabbf43492e
slappyhammer <mirza_andrei@yahoo.com>
2022-11-11 16:13:41 +0200

    Lab3 initial

---
+++ Models/AssignedCategoryData.cs
@@ -0,0 +1,9 @@
+﻿namespace Mirza_Andrei_Lab2.Models
+{
+    public class AssignedCategoryData
+    {
+        public int CategoryID { get; set; }
+        public string Name { get; set; }
+        public bool Assigned { get; set; }
+    }
+}
